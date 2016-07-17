c6d144d3682d8eda70e91f11cb0c2481467aafd6
Equipogithub <jthernandezlaurel@hotmail.com>
2016-07-17 17:11:28 +0200

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,6 @@
+<?php    
+	define("DB_TYPE", "mysql");
+    define("DB_HOST", "localhost:3306");
+    define("DB_NAME", "terceroa_Alberto");
+    define("DB_USER", "terceroa_alberto");
+    define("DB_PASS", "Hoyesmartes1"); 
--- usuarios.php
+++ usuarios.php
@@ -1,182 +1,120 @@
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Pagina Anime</title>
-	<link rel="stylesheet" type="text/css" href="general.css">
-</head>
-		
-<body div   background="img/chica.jpg">
-<div class="caja">
-     <div class="banner"></div>
+<?php
+require_once 'control/datos.php';
+
+class usuarios {
+    protected $connect;
+    protected $db;
+    // Attempts to initialize the database connection using the supplied info.
+   function __construct() {
+        $this->connect = mysql_connect(DB_HOST, DB_USER, DB_PASS);
+        $this->db = mysql_select_db(DB_NAME);
+   }
     
-    <div class="menu"> 
-	<nav>
-		<ul>
-      <li><a href="#" class="Inicio">Concerts</a></li>        
-    <li><a href="#" class="Manga">News</a></li>        
-    <li><a href="#" class="Peliculas">Radio</a></li>
-    <li><a href="#" class="videos">Videos</a></li>        
-    <li><a href="#" class="Ovas">Mixes </a></li>         
-    <li><a href="#" class="music">Music</a></li>        
-    <li><a href="#" class="Club">Featured</a></li>        
-    <li><a href="#" class="Salir">Welcome</a></li> 
-            <a href="http://tic3ajosealberto.blogspot.mx/2016/06/jose-alberto-hernandezlaurel-las-palmas.html" target="_blank" > Ir a blog</a>
-          
-		</ul>	
-	</nav>
-        
-             
-    
-    </div>
-         <div class="contenido"> Contenido de la pagina
-           
-            <aside id="datos">
-		<form id="formulario2" action="proceso.php" method="POST" >
-		<table >
-		<tr>
-			<td> <label for="usuario">Usuario :</label></td>
-			<td><input type="text" id="txtusr" name="txtusr" placeholder="Usuario" autofocus></td>
-		</tr>  
-		<tr>
-			<td><label for="passwrd">Password :</label></td>
-            
-			<td><input type="password" id="txtpwd" name="txtpwd" ></td>
-		</tr>
-		<tr>
-			<td><input type="submit" name="btn" value="Aceptar" id="btn"></td>
-            <div class="eliminar"> 
-                <td><input type="submit" name="btn" value="Eliminar" id="btn"></td>  
-            </div>
-		</tr>
-        </table>
-            </form>
-            <div class="formulario">
-		<table>
-		
-	  
-            <?php
-            require_once
-            'control/usuarios.php';
-            $clsUsuarios = new usuarios();
-            $dsUsuarios= $clsUsuarios->getUsers();
-            $tablausuarios="<table id='listausuarios'>";
-            $tablausuarios=$tablausuarios."<thead>";
-            $tablausuarios=$tablausuarios."<tr>";
-            $tablausuarios=$tablausuarios."<th></th>";
-            $tablausuarios=$tablausuarios."<th>ID</th>";
-            $tablausuarios=$tablausuarios."<th>USUARIOS</th>";
-                $tablausuarios=$tablausuarios."</tr>";
-            $tablausuarios=$tablausuarios."</thead>";
-            $tablausuarios=$tablausuarios."<tbody>";
-            if(sizeof($dsUsuarios) >0){
-                foreach($dsUsuarios as $usuario){
-                    
-                    $tablausuarios=$tablausuarios."<tr>";
-                    $tablausuarios=$tablausuarios."<td><input type='checkbox' name='seleccionar' id='".$usuario->getId()."'/></td>";
-                    
-                    
-                    $tablausuarios=$tablausuarios."<td>".$usuario->getId().'</td>';
-                    $tablausuarios=$tablausuarios."<td>".$usuario->getUsername().'</td>';
-                    $tablausuarios=$tablausuarios."</tr>";
-                    
-                }
-            }else{
-                $tablausuarios=$tablausuarios."<tr>";
-                $tablausuarios=$tablausuarios."<td>No existen Usuarios</td>";
-                $tablausuarios=$tablausuarios."</tr>";
-                
+    // Executes the specified query and returns an associative array of reseults.
+    protected function execute($sql) {
+        $res = mysql_query($sql, $this->connect) or die(mysql_error());
+		$userVO=null;
+        if(mysql_num_rows($res) > 0) {
+            for($i = 0; $i < mysql_num_rows($res); $i++) {
+                $row = mysql_fetch_assoc($res);
+                $userVO[$i] = new UserVO();
+				//echo $row["usrid"]." ".$row["usrnombre"]." ".$row["usrpass"];
+                $userVO[$i]->setId($row["id"]);
+                $userVO[$i]->setUsername($row["username"]);
+                $userVO[$i]->setPassword($row["password"]);
             }
-            
-                $tablausuarios=$tablausuarios."</tbody>";
-                $tablausuarios=$tablausuarios."</table>";
-?>
-                
-            <div style="text-align:right">
-            <button id="btnBorrar">Eliminar Registro</button>
-			</div>
-			<?php echo $tablausuarios; ?>
-
-                
-    </table>
-    </div>            
-    </div>
-
-<div class="destacados"> Area de destacado y relacionado
-	
-            <section>
-		<article>
-
-		</article>
-	</section>
-	
-	<footer></footer>
-            
-            
- </div>
+        }
+        return $userVO;
+	}
     
-   
+    // Retrieves the corresponding row for the specified user ID.
+    public function getByUserId($userId) {
+        $sql = "SELECT * FROM users WHERE id=".$userId;
+        return $this->execute($sql);
+    }
     
-    <div class="gl"> 
+    // Retrieves all users currently in the database.
+    public function getUsers() {
+        $sql = "SELECT * FROM users";
+        return $this->execute($sql);
+    }
     
-
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1869.8141202127053!2d-101.22373849190843!3d20.398212446585816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842c8c58dcd0ed3d%3A0x7d052b46eb2332e9!2sUTSOE%2C+Ecologista%2C+38400+Valle+de+Santiago%2C+Gto.!5e0!3m2!1ses!2smx!4v1467813356795" width="750" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+    // Retrieves all users currently in the database.
+    public function getByUserPass($usuario,$pass) {
+        $sql = "SELECT * FROM datos WHERE username='".$usuario."' AND password='".$pass."'";
+        return $this->execute($sql);
+    }
+	  
+    //Saves the supplied user to the database.
+    public function save($userVO) {
+        $affectedRows = 0;
         
+        if($userVO->getId() != "") {
+            $currUserVO = $this->getByUserId($userVO->getId());
+        }
+        // If the query returned a row then update,
+        // otherwise insert a new user.
+        if(sizeof($currUserVO) > 0) {
+            $sql = "UPDATE users SET ".
+                "username='".$userVO->getUsername()."', ".
+                "password='".$userVO->getPassword()."' ".
+                "WHERE id=".$userVO->getId();
+            
+            mysql_query($sql, $this->connect) or die(mysql_error());
+            $affectedRows = mysql_affected_rows();
+        }
+        else {
+            $sql = "INSERT INTO users (username, password) VALUES('".
+                $userVO->getUsername()."', ".
+                $userVO->getPassword()."')".
+            
+            mysql_query($sql, $this->connect) or die(mysql_error());
+            $affectedRows = mysql_affected_rows();
+        }
+        return $affectedRows;
+    }
+    
+    // Deletes the supplied user from the database.
+    public function delete($userVO) {
+        $affectedRows = 0;
+        
+        // Check for a user ID.
+        if($userVO->getId() != "") {
+            $currUserVO = $this->getByUserId($userVO->getId());
+        }
+        
+        // Otherwise delete a user.
+        if(sizeof($currUserVO) > 0) {
+            $sql = "DELETE FROM users WHERE id=".$userVO->getId();
+            
+            mysql_query($sql, $this->connect) or die(mysql_error());
+            $affectedRows = mysql_affected_rows();
+        
+        return $affectedRows;
+    }
+  }
+}
 
-    </div>
-    
-    <div class="pie">Pie de pagina</div>
-    </div>    
-<script src="public/plugins/jquery-2.1.4.min.js"></script>
-<script>
-$(document).ready(function(){
-	// this is the id of the form
-	$("#datos").find("form").on("submit", function (event) {
-     event.preventDefault();
-		$.ajax({
-          url: "proceso.php", 
-          type: "POST",
-          //datos del formulario
-          data: $(this).serialize(),
-          //una vez finalizado correctamente
-          success: function (response) {
-		 	if(response!=""){
-				alert(response); 
-				// show response from the php script.	
-			}
-          }
-     });
-	});
-    
-    
-    $("#btnBorrar").click(function (event) {
-		$("input:checkbox[name=seleccionar]:checked").each(function() {
-          var parametros = {
-          		"ID": $(this).attr("id")
-		  	  };
-			
-		  $.ajax({
-	      url: "borrar.php", 
-          type: "POST",
-          //datos del formulario
-          data: parametros,
-          //una vez finalizado correctamente
-          success: function (response) {
-               location.reload();
-		  },
-          error: function (response) {
-               alert(response);
-		  },
-		  });
-
-    	});
-		//window.location.href = path + 'xls/articulosCica2016.xlsx';
-     });
-
-    
-});
-     
-    
-</script>
-</body>
-</html>
+class UserVO {
+	protected $id;
+	protected $username;
+	protected $password;
+	public function setId($id) {
+	$this->id = $id;
+	}
+	public function getId() {
+	return $this->id;
+	}
+	public function setUsername($username) {
+	$this->username = $username;
+	}
+	public function getUsername() {
+	return $this->username;
+	}
+	public function setPassword($password) {
+	$this->password = $password;
+	}
+	public function getPassword() {
+	return $this->password;
+	}
+}
